dd3ba507ca3e5d144b44f76289b653ea305c70b3
shadowkyros <randall.vowles@utah.edu>
2017-04-21 12:31:22 -0600

    built metadata emitter, started table emitter

--- _src/apps/firemonitor.js
+++ _src/apps/firemonitor.js
@@ -48,15 +48,33 @@
 
             fireListingService: "//home.chpc.utah.edu/~u0751826/fireapi/listing",
             fireDataService: "//home.chpc.utah.edu/~u0751826/fireapi/fires",
-            self: "firemonitor.html",
+            // self: "firemonitor.html",
+            self: "//127.0.0.1:4000/firemonitor.html?fire=",
             // qcNetworkURL: "./network.html",
             mwHome: "//mesowest.utah.edu",
-            openInNewWindow: true
+            openInNewWindow: true,
+            geoMacc: "//rmgsc.cr.usgs.gov/outgoing/GeoMAC/"
         },
         toolbar: {
             id: "toolbar-local-tools-container"
         },
         fireMonitorEmitter: {
+            headers: [
+                {apiName: "stid", title: "Station ID", shortname: "STID"},
+                {apiName: "air_temp", title: "Air Temperature", shortname: "AT"},
+                {apiName: "relative_humidity", title: "Relative Humidity", shortname: "RH"},
+                {apiName: "wind_speed", title: "Wind Speed", shortname: "WS"},
+                {apiName: "wind_gust", title: "Wind Gust", shortname: "WG"},
+                {apiName: "wind_direction", title: "Wind Direction", shortname: "WD"},
+                {apiName: "weather_condition", title: "Weather Condition", shortname: "WC"},
+                {apiName: "bfp", title: "Bearing From Perimeter", shortname: "BFP"},
+                {apiName: "dfp", title: "Distance From Perimeter", shortname: "DFP"},
+                {apiName: "time", title: "Time From Observation", shortname: "time"}
+            ],
+            headerTOC: {
+                stid: 0, dfp: 8, bfp: 7, time: 9, air_temp: 1, relative_humidity: 2,
+                wind_speed: 3, wind_gust: 4, wind_direction: 5, weather_condition: 6
+            },
             props: {
                 containerId: "firemonitor-container",
                 tableId: "firemonitor",
@@ -75,7 +93,7 @@
         fireMetadataEmitter: {
             props: {
                 containerId: "fire-metadata-container",
-                tableId: "fire-metadata",
+                id: "fire-metadata",
                 classList: "fire-metadata-table table table-condensed table-bordered"
             }
         },
@@ -94,7 +112,7 @@
     var _CLASS_ = ".";
 
     // Start off by hiding app while we load the canons
-    d3.selectAll(_ID_ + state.ui.appContainer.id).classed(state.ui.css.hide, true);
+    // d3.selectAll(_ID_ + state.ui.appContainer.id).classed(state.ui.css.hide, true);
 
     // Get the user's cookie, determine if the cookie needs to be replaced or not
     state.P = new User({
@@ -128,6 +146,8 @@
     state.api.sensorvars = 1;
     state.api.complete = 1;
     state.api.recent = "61"
+    var rankedSensors = ["air_temp", "relative_humidity", "wind_speed", "wind_gust", "wind_direction", "weather_condition"]
+    state.api.vars = rankedSensors.join(",");
     // state.api.precip = 1;
     // state.api.pmode = "interval";
     state.api.units = state.P.getUnits();
@@ -136,10 +156,8 @@
         state.apiToken = state.http.thisURL.token;
         Mesonet.setToken(state.apiToken);
     }
-
     var stidStack = [];
     var stidAndDist = [];
-
 
 
     // Time to fetch the json objects
@@ -147,18 +165,18 @@
         state.store.fireCache = data;
         state.store.defaultFireId = Object.keys(data.CURRENT_FIRES)[0];
         var windowUrl = getWindowArgs();
-        var fireId = windowUrl.fire !== "undefined" ? windowUrl.fire : state.store.defaultFireId;
+        var fireId = typeof windowUrl.fire !== "undefined" ? windowUrl.fire : state.store.defaultFireId;
         state.store.current_fireId = fireId
         console.log('fireId', fireId);
         console.log('Fire Metadata Cache', data);
         //maybe send the keys to the dropdown menu at this point
         _buildDropdownFireSelect();
-        jsonFetch(state.http.fireDataService, { id: state.store.defaultFireId }, function (d) {
+        jsonFetch(state.http.fireDataService, { id: fireId }, function (d) {
             state.store.fullFireData = d;
             console.log('Specific Fire Data', d);
-            // console.log(state.store)
+            console.log(state.store)
             var key;
-            var _d = d.FIRES[state.store.defaultFireId].nearest_stations
+            var _d = d.FIRES[fireId].nearest_stations
             for (key in _d) {
                 stidStack.push(_d[key].STID);
                 stidAndDist.push(_d[key].DFP);
@@ -168,8 +186,12 @@
             Mesonet.fetch({
                 apiArgs: state.api
             })
+            $.when(Mesonet.ready()).done(function(){
+                 haveResponse();
+            })
         })
     })
+
 
     // Show response in console?
     parseRunFlags('showResponse', '1', null, true, function () { console.log(Mesonet); });
@@ -192,16 +214,17 @@
     }
     // Update the user's time format setting
     state.fireMonitorEmitter.props.timeUTC = state.P._isUTC();
-
     // While we are waiting, let's generate the toolbar links.
     populateToolbar();
 
-    $.when(Mesonet.ready()).done(function () {
+
+
+    function haveResponse(){
         // Check the API response to make sure we have data and not just an error message.
         console.log("API response", Mesonet.store)
         if (Mesonet.store.status !== 1) {
             d3.select(_ID_ + state.ui.loading.progressBarId).classed(state.ui.css.hide, true);
-            d3.select(_ID_ + state.ui.loading.errMessageId).text("Rhut Rho!");
+            d3.select(_ID_ + state.ui.loading.errMessageId).text("Danger Danger!");
             createAlertBox({
                 alertType: "danger",
                 message: "Error code: <b>" + Mesonet.store.telemetry[0] + "<b/>",
@@ -213,9 +236,30 @@
         d3.selectAll(_ID_ + state.ui.appContainer.id).classed(state.ui.css.hide, false);
         d3.select(_ID_ + state.ui.loading.id).classed(state.ui.css.hide, true);
 
-    })
+        // table and metadata emitters go here
+        fireMetadataEmitter();
 
 
+
+}
+
+    function fireMonitorEmitter(){
+        var rankedSensorStack = Object.keys(state.fireMonitorEmitter.headerTOC);
+        state.store.rankedSensorStack = rankedSensorStack;
+        var _M = Mesonet.store;
+        var _F = state.store.fullFireData;
+        var props = state.fireMonitorEmitter.props;
+        var DisplayUnits = new Units();
+        var tableData = [];
+        _M.station.map(function (q){
+            var last = _M[q].OBSERVATIONS.date_time.length -1;
+            var temp = {};
+            rankedSensorStack.map(function (d) {
+                
+            })
+
+        })
+    }
 
 
 
@@ -228,7 +272,7 @@
                 d3.select(this)
                     .append("li")
                     .append("a")
-                    .attr("href", "//127.0.0.1:4000/firemonitor.html?fire=" + key)
+                    .attr("href", state.http.self + key)
                     .text(key)
             })
         }
@@ -342,5 +386,38 @@
         document.getElementById(state.toolbar.id).innerHTML = a;
     }
 
+    function hrefHandler(url, newWindow) {
+        if ((typeof newWindow !== "undefined" && newWindow) || state.http.openInNewWindow) {
+            window.open(url, '_blank');
+        }
+        else {
+            window.location.href = url;
+        }
+    }
+
+    /** Render table & update text fields */
+    function fireMetadataEmitter() {
+        var props = state.fireMetadataEmitter.props;
+        // I can't wait to have my new DOM tools in place.  Make this thing a lot nicer.
+        // console.log("here", state.store.fullFireData, state.store.current_fireId)
+        var _f = state.store.fullFireData["FIRES"][state.store.current_fireId];
+        var _s = Mesonet.store.station[0];
+
+        d3.select(_ID_ + props.id).append("h3").text(_f.name);
+        d3.select(_ID_ + props.id).append("p").text(
+            _s.COUNTY + " County, " + _s.STATE + ", " + _s.COUNTRY
+        );
+        d3.select(_ID_ + props.id).append("p").text(
+            _f.lat + " N, " + _f.lon + " E" 
+            // + ", Elevation " + _s.ELEVATION + " ft."
+        );
+
+        d3.select(_ID_ + props.id).append("p").append("a")
+            .on('click', function (d) {
+                var url = state.http.geoMacc
+                hrefHandler(url, true);
+            })
+            .text("Data accessed from GeoMAC database");
+    }
 
 })();
